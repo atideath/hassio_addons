794a3d57e3326ecb56991fe8bc411f50e05d6dec
alexxx113 <lead1100@ya.ru>
2022-09-02 21:06:36 +0300

    Update run.sh

--- example/alarmserver/run.sh
+++ example/alarmserver/run.sh
@@ -2,9 +2,9 @@
 
 echo "start 8000 web"
 
-python3 -m http.server 8000
+python3 -m http.server 8000 &
 
 echo "start alarmserver"
-/alarmserver-linux-raspberry
+/alarmserver-linux-raspberry &
 
 echo "alarmserver run"
