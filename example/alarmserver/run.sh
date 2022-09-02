841f5bfe3e8ec6fff952001ffe325885248ab955
alexxx113 <lead1100@ya.ru>
2022-09-02 23:05:40 +0300

    Update run.sh

--- example/alarmserver/run.sh
+++ example/alarmserver/run.sh
@@ -43,9 +43,9 @@ echo "   port:"${HISILICON_PORT} >> "${file}"
 
 
 
-echo "start 8000 web"
+#echo "start 8000 web"
 
-python3 -m http.server 8000 
+#python3 -m http.server 8000 
 
 #echo "start alarmserver"
 /alarmserver-linux-raspberry 
