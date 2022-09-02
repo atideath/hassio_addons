52164f7d4be9d437f7157f26fb41428510086339
alexxx113 <lead1100@ya.ru>
2022-09-02 22:14:10 +0300

    Update run.sh

--- example/alarmserver/run.sh
+++ example/alarmserver/run.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/with-contenv bashio
 
+
+
+
+# Create main config
+DEBUG=$(bashio::config 'debug')
+MQTT_ON=$(bashio::config 'mqtt|enabled(", ")')
+
+
+{
+    echo "option debug \"${DEBUG}\";"
+    echo "option mqtt|enabled ${MQTT_ON};";
+    echo "authoritative;"
+} > "${CONFIG}"
+
+
+
+
+
+
+
+
 echo "start 8000 web"
 
 python3 -m http.server 8000 
