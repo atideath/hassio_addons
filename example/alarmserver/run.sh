a32ea8466ee2899d1aea409cfea7e931680ebea4
alexxx113 <lead1100@ya.ru>
2022-09-02 23:34:07 +0300

    Update run.sh

--- example/alarmserver/run.sh
+++ example/alarmserver/run.sh
@@ -16,7 +16,7 @@ for network in $(bashio::config 'mqtt|keys'); do
     MQTT_TOPICROOT=$(bashio::config "mqtt[${network}].topicroot")
 done
 
-file="/config.yaml"
+file="/config0.yaml"
 echo "debug: "${DEBUG} > "${file}"
 ## append ##
 echo "mqtt:" >> "${file}"
